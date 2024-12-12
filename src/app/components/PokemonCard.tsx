b2b51095163469c616415556ad53142ca1f62d2a
Aryan <malhotraaryan570@gmail.com>
2024-12-12 14:34:38 +0530

    width_setting

--- src/app/components/PokemonCard.tsx
+++ src/app/components/PokemonCard.tsx
@@ -27,48 +27,48 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, type, sprite
     return (
       <div onMouseLeave={() => setComp(false)} className={`flex items-center p-4 h-[216px] bg-white shadow-md rounded-lg hover:shadow-lg hover:scale-105 ${mpp2.get(type)}`}>
         <Image src={sprite} alt={name} className="w-24 h-24" width={150} height={150} />
-        <div className="ml-2 flex flex-col gap-2">
-          <div className="flex justify-end items-center gap-1">
+        <div className="ml-2 flex flex-col gap-2 w-2/3">
+          <div className="flex justify-end items-center gap-1 w-full">
             <div className="text-xs">health</div>
-            <div className="relative h-2 bg-gray-200 rounded w-40">
+            <div className="relative h-2 bg-gray-200 rounded w-full">
               <div
                 className="absolute top-0 left-0 h-full bg-emerald-600 rounded"
                 style={{ width: `${values[0]}%` }}
               />
             </div>
           </div>
-          <div className="flex justify-end items-center gap-1">
+          <div className="flex justify-end items-center gap-1 w-full">
             <div className="text-xs">stamina</div>
-            <div className="relative h-2 bg-gray-200 rounded w-40">
+            <div className="relative h-2 bg-gray-200 rounded w-full">
               <div
                 className="absolute top-0 left-0 h-full bg-emerald-600 rounded"
                 style={{ width: `${values[1]}%` }}
               />
             </div>
           </div>
-          <div className="flex justify-end items-center gap-1">
+          <div className="flex justify-end items-center gap-1 w-full">
             <div className="text-xs">defense</div>
-            <div className="relative h-2 bg-gray-200 rounded w-40">
+            <div className="relative h-2 bg-gray-200 rounded w-full">
               <div
                 className="absolute top-0 left-0 h-full bg-emerald-600 rounded"
                 style={{ width: `${values[2]}%` }}
               />
             </div>
           </div>
-          <div className="flex justify-end items-center gap-1">
+          <div className="flex justify-end items-center gap-1 w-full">
             <div className="text-xs">speed</div>
-            <div className="relative h-2 bg-gray-200 rounded w-40">
+            <div className="relative h-2 bg-gray-200 rounded w-full">
               <div
                 className="absolute top-0 left-0 h-full bg-emerald-600 rounded"
                 style={{ width: `${values[3]}%` }}
               />
             </div>
           </div>
-          <div className="flex justify-end items-center gap-1" >
+          <div className="flex justify-end items-center gap-1 w-full" >
             <div className="text-xs">attack</div>
-            <div className="relative h-2 bg-gray-200 rounded w-40">
+            <div className="relative h-2 bg-gray-200 rounded w-full">
               <div
-                className="absolute top-0 left-0 h-full bg-emerald-600 rounded"
+                className="absolute top-0 left-0 h-full bg-emerald-600 rounded w-2/3"
                 style={{ width: `${values[4]}%` }}
               />
             </div>
